88e410b1821c7a4d5e126f67f94de8b75b3b72a8
Christian Yang <christian.yang@futuregames.nu>
2022-02-18 23:57:18 -0800

    Create basic editor inputs

--- platformer_demo/Assets/Editor/EaseToolEditor.cs
+++ platformer_demo/Assets/Editor/EaseToolEditor.cs
@@ -1,23 +1,31 @@
-using System.Collections;
+
 using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEditor;
 
+using System;
+using System.Linq;
+
 [CustomEditor(typeof(EaseTool))]
 public class EaseToolEditor : Editor
 {
     float gridSize = 0.0f;
+    int index = 0;
+
+    enum fields { Select }
+
+    private void OnEnable()
+    {
+
+    }
+
     public override void OnInspectorGUI()
     {
+        EaseTool current = (EaseTool)target;
+
         base.OnInspectorGUI();
-
         GUILayout.Space(10);
-        GUILayout.Label("Rank function: ");
-        GUILayout.Space(5);
-        GUILayout.Label("Output Variables Num: ");
-        GUILayout.Space(5);
-
         using (new GUILayout.HorizontalScope())
         {
             GUILayout.Label("Grid Size: ");
@@ -26,6 +34,53 @@ public class EaseToolEditor : Editor
 
             }
         }
+
         GUILayout.Space(10);
+        GUILayout.Label("Rank function: ");
+        GUILayout.Space(10);
+
+
+
+
+
+        current.numOutput = EditorGUILayout.IntField("Output Num: ", current.numOutput);
+
+        for (int i = 0; i < current.numOutput; i++)
+        {
+            GUILayout.Space(5);
+            current.test = (GameObject)EditorGUILayout.ObjectField(current.test, typeof(UnityEngine.Object), true);
+
+
+
+            var componentArr = current.test.GetComponents(typeof(Component));
+            String[] componentNames = new String[componentArr.Length];
+            for (int j = 0; i < componentArr.Length; i++)
+            {
+                componentNames[j] = componentArr[i].GetType().ToString();
+            }
+            current.printArr(componentNames);
+
+
+            // // var propertyValues = current.test.GetType().GetFields();
+            // var propertyValues = current.test.GetType().GetProperties();
+
+
+
+            // // FieldInfo[] valuesArr = new FieldInfo[propertyValues.Length];
+            // String[] names = new String[propertyValues.Length];
+            // for (int j = 0; i < propertyValues.Length; i++)
+            // {
+            //     names[j] = propertyValues[j].Name;
+            // }
+            // current.printArr(names);
+            // index = EditorGUILayout.Popup(index, names);
+
+        }
+        GUILayout.Space(10);
+
+
+        GUILayout.Space(10);
+
+
     }
 }
--- platformer_demo/Assets/Scripts/EaseTool.cs
+++ platformer_demo/Assets/Scripts/EaseTool.cs
@@ -1,13 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EaseTool : MonoBehaviour
 {
+    public GameObject test;
+    public int hello;
+    public int hello2;
+
+    public GameObject[] paras;
+    public int numOutput;
+    // public Fu rewardFunc;
+
     // Start is called before the first frame update
     void Start()
     {
+        print("");// yo
+    }
 
+    public void printArr(String[] arr)
+    {
+        print("Printing arr " + arr.Length);
+        for (int i = 0; i < arr.Length; i++)
+        {
+            print(arr[i]);
+        }
     }
 
     // Update is called once per frame
