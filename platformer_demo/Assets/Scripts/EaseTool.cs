93143e6c12ae02e7b494b0ded5059eb1687db290
al1729 <andyliu1708@gmail.com>
2022-02-18 21:35:37 -0800

    param class

--- platformer_demo/Assets/Scripts/EaseTool.cs
+++ platformer_demo/Assets/Scripts/EaseTool.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Param;
 
 public class EaseTool : MonoBehaviour
 {
+    public Param[] paras;
+    public func rewardFunc;
     // Start is called before the first frame update
     void Start()
     {
---
+++ platformer_demo/Assets/Scripts/Param.cs
@@ -0,0 +1,37 @@
+public class Param
+{
+    /* it definitely makes since to make param its own class to store potential metadata
+    in addition to what I have below. not sure if this is the exact setup we want though
+    */
+        public int currentValue;
+    public int defaultValue;
+    public string name;
+    public Param(int currentValue, int defaultValue, string name) {
+        this.currentValue = currentValue;
+        this.defaultValue = defaultValue;
+        this.name = name;
+    }
+
+    public Param(int defaultValue, string name) {
+        this.currentValue = defaultValue;
+        this.defaultValue = defaultValue;
+        this.name = name;
+    }
+
+    void updateValue(int newValue) {
+        this.currentValue = newValue;
+    }
+
+    int getDefault() {
+        return this.defaultValue;
+    }
+
+    int getValue() {
+        return this.currentValue;
+    }
+
+    void returnToDefault() {
+        this.currentValue = this.defaultValue;
+    }
+
+}
