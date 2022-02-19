1d6cbfc4c709f7d6767397882be6357c00e0e594
Christian Yang <christian.yang@futuregames.nu>
2022-02-18 18:41:30 -0800

    Basic pipeline for the tool

---
+++ platformer_demo/Assets/Editor/EaseToolEditor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EaseTool))]
+public class EaseToolEditor : Editor
+{
+    float gridSize = 0.0f;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GUILayout.Space(10);
+        using (new GUILayout.HorizontalScope())
+        {
+            GUILayout.Label("Grid Size: ");
+            if (GUILayout.Button("Snap Selecttion"))
+            {
+
+            }
+        }
+        GUILayout.Space(10);
+    }
+}
